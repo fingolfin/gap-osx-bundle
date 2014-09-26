100f60530ec000fb457853285a046abcd14bf997
Max Horn <max@quendi.de>
2014-09-26 16:19:54 +0200

    Tweak GMP to install a generic version

--- gmp/compile.sh
+++ gmp/compile.sh
@@ -1,4 +1,16 @@
 #!/bin/sh -ev
 
-./configure --prefix=$PREFIX
+# We override the host type to build a GMP version that works across a
+# wide range of OS X machines, avoiding CPU specific optimizations. Of
+# course this produces somewhat slower binaries, but at least it should
+# work everywhere.
+# In particular, we fake a Core Duo CPU with Mac OS X 10.6
+
+./configure --prefix=$PREFIX \
+    --host=core2-apple-darwin10.0.0 \
+    --disable-static \
+    --enable-cxx \
+    --with-pic
 make
+
+make check
