50da98d2dbef49d9738c603e6a55f13fe8c731fa
Max Horn <max@quendi.de>
2014-09-28 16:14:54 +0200

    mpfi: strip installed dylib

--- mpfi/install.sh
+++ mpfi/install.sh
@@ -5,6 +5,7 @@ make install
 rm -f $PREFIX/lib/*.la
 
 for name in libmpfi.0.dylib ; do
+    strip -S $PREFIX/lib/$name
     install_name_tool -id @rpath/$name $PREFIX/lib/$name
     install_name_tool -rpath $PREFIX/lib "../Resources/lib" $PREFIX/lib/$name
 done
