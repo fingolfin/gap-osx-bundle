baf7339b7a923a25467be5437952e18fe254631e
Max Horn <max@quendi.de>
2014-09-28 13:38:24 +0200

    Improve fix_install_names.sh

--- fix_install_names.sh
+++ fix_install_names.sh
@@ -1,46 +1,52 @@
-#!/bin/sh -ex
+#!/bin/sh -e
 #
-# Take a binary (shared library or executable) and run install_name_tool
-# on it to fixup its id (if it is a dylib), its default rpath, and
-# the location of any dependant shared libs, substituting OLDPREFIX by NEWPREFIX.
+# Take one or multiple binaries (shared libraries or executables) and
+# run install_name_tool on them to adjust the id (for dylibs), the
+# default rpath (if necessary), and the location of any dependant shared
+# libs, substituting OLDPREFIX by NEWPREFIX.
 
 usage() {
     cat <<EOF
-Usage: $0 BINARY OLDPREFIX NEWPREFIX
+Usage: $0 OLDPREFIX NEWPREFIX  BINARY [BINARIES...]
 EOF
 
     exit $1
 }
 
-if [ $# -ne 3 ]; then
+if [ $# -lt 3 ]; then
     usage 1
 fi
 
-BINARY=$1
-OLDPREFIX=$2
-NEWPREFIX=$3
+OLDPREFIX=$1
+NEWPREFIX=$2
 
-if [ ! -f $BINARY ] ; then
-    usage 1
-fi
+shift 2
 
-# If it is a shared library, fix its id
-if [[ $BINARY =~ \.dylib$ ]] ; then
-    install_name_tool -id $NEWPREFIX/`basename $BINARY` $BINARY
-fi
+# Iterate over all arguments
+for BINARY ; do
 
-# For everything: Fix the default rpath
-# TODO: Some binaries may not have any rpath set; for these, the below command
-# will error out... ignore that???
-# It is also a bit ugly that we hardcode "../Resources/lib"
-install_name_tool -rpath $OLDPREFIX "../Resources/lib" $BINARY
+    if [ ! -f $BINARY ] ; then
+        usage 1
+    fi
 
-# Finally, fixup the locations of dependant libraries
+    # If it is a shared library, fix its id
+    if [[ $BINARY =~ \.dylib$ ]] ; then
+        install_name_tool -id $NEWPREFIX/`basename $BINARY` $BINARY
+    fi
 
-libs=`otool -L $BINARY | sed -e 1d | fgrep $OLDPREFIX | sed -E -e 's/[[:space:]]+(.+dylib) .+/\1/'`
-for lib in $libs ; do
-    newlib=`echo $lib | sed -e "s,$OLDPREFIX,$NEWPREFIX,"`
-    install_name_tool -change $lib $newlib $BINARY
+    # For everything: Fix the default rpath
+    # TODO: Some binaries may not have any rpath set; for these, the below command
+    # will error out... for now we ignore that.
+    # TODO: It is also a bit ugly that we hardcode "../Resources/lib"
+    install_name_tool -rpath $OLDPREFIX "../Resources/lib" $BINARY 2>/dev/null || echo "rpath not changed"
+
+    # Finally, fixup the locations of dependant libraries
+    libs=`otool -L $BINARY | sed -e 1d | fgrep $OLDPREFIX | sed -E -e 's/[[:space:]]+(.+dylib) .+/\1/'`
+    for lib in $libs ; do
+        newlib=`echo $lib | sed -e "s,$OLDPREFIX,$NEWPREFIX,"`
+        install_name_tool -change $lib $newlib $BINARY
+    done
+
 done
 
 exit 0
