161f843af5ae3135ac5bb3621c2d960c51acff9c
Max Horn <max@quendi.de>
2014-09-28 13:28:39 +0200

    singular: apply a patch which prevents most (all?) hardcoded paths

--- singular/compile.sh
+++ singular/compile.sh
@@ -3,6 +3,8 @@
 # Tell singular explicitly were to look for each library. Otherwise
 # it may pick up libraries from /opt/local or /sw
 ./configure --prefix=$PREFIX \
+    --disable-maintainer-mode \
+    --disable-dependency-tracking \
     --disable-static \
     --with-gmp=$PREFIX \
     --with-flint=$PREFIX \
