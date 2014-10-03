d7f04b0f63ba4fd72b8bf6990117472742352072
Max Horn <max@quendi.de>
2014-10-03 18:56:50 +0200

    gap: fix error in install.sh

--- gap/install.sh
+++ gap/install.sh
@@ -3,7 +3,7 @@
 DEST=$PREFIX/lib/gap
 cd $DEST
 
-echo TODO: adjust bin/gap.sh; install into $PREFIX/bin; 
+echo "TODO: adjust bin/gap.sh; install into $PREFIX/bin; "
 
 echo "TODO: fix install_name"
 
