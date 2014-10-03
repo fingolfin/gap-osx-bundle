9065d9e87fc24ae9dda8052f27b04ae604451148
Max Horn <max@quendi.de>
2014-10-03 15:23:17 +0200

    normaliz: fix path to libnormaliz.dylib in bin/normaliz

--- normaliz/install.sh
+++ normaliz/install.sh
@@ -4,3 +4,7 @@ cd source
 make install
 
 $BASEDIR/fix_install_names.sh $PREFIX bin/normaliz lib/libnormaliz.dylib 
+
+# For some strange reason, normaliz ends up using @rpath/libnormaliz.0.dylib
+# even before we run fix_install_names.sh, so we must fix bin/normaliz manually.
+install_name_tool -change @rpath/libnormaliz.0.dylib @rpath/lib/libnormaliz.0.dylib $PREFIX/bin/normaliz
