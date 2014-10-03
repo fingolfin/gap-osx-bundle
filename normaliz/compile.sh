de5b01a21e7054bee3b0a809c14a0b60e6040551
Max Horn <max@quendi.de>
2014-10-03 14:56:40 +0200

    normaliz: link against our GMP, not Fink's

--- normaliz/compile.sh
+++ normaliz/compile.sh
@@ -4,5 +4,5 @@ cd source
 # HACK: invoke cmake installed by fink.
 # Strictly speaking, we should probably provide cmake, too...
 # but this seems to much effort for no visible gain.
-/sw/bin/cmake -DCMAKE_INSTALL_PREFIX:PATH="$PREFIX"
+GMP_DIR="$PREFIX" /sw/bin/cmake -DCMAKE_INSTALL_PREFIX:PATH="$PREFIX"
 make
