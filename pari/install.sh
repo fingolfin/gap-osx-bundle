ad774fc933c233b3127b8b1cb13daff0789f7390
Max Horn <max@quendi.de>
2014-10-03 15:29:02 +0200

    pari: cleanup TODO in install.sh

--- pari/install.sh
+++ pari/install.sh
@@ -6,16 +6,16 @@ make install
 
 $BASEDIR/fix_install_names.sh $PREFIX bin/gp-2.7 lib/libpari-gmp.dylib
 
-# TODO: the BUNDLE path is still being referenced in the following files:
-# bin/gp-2.7
+# TODO: libpari-gmp.dylib references the path
+#   $PREFIX/share/pari
+
+# TODO: bin/gp-2.7 references the path
+#   $PREFIX/bin/gbhelp
+
+# TODO: the BUNDLE path is also being referenced in the following files:
 # bin/gphelp
 # include/pari/paricfg.h
 # lib/pari/pari.cfg
 # share/pari/doc/paricfg.tex
 # share/pari/examples/Makefile
 
-# TODO: libpari-gmp.dylib references the path
-#   $PREFIX/share/pari
-
-# TODO: bin/gp-2.7 references the path
-#   $PREFIX/bin/gbhelp
