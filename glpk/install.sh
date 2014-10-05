1eb2bda7a350494bdbad7dc3482cb2d40758e8ab
Max Horn <max@quendi.de>
2014-10-05 20:20:15 +0200

    glpk: fix install.sh

--- glpk/install.sh
+++ glpk/install.sh
@@ -4,4 +4,4 @@ make install
 
 rm -f $PREFIX/lib/*.la
 
-$BASEDIR/fix_install_names.sh $PREFIX bin/glpsol lib/libglpk.0.dylib lib/libglpk.36.dylib
+$BASEDIR/fix_install_names.sh $PREFIX bin/glpsol lib/libglpk.36.dylib
