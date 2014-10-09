62e87d15783470e5439800bb471b62f82f8c5072
Max Horn <max@quendi.de>
2014-10-09 14:09:12 +0200

    pari: install PREFIX/etc/gprc and PREFIX/etc/gpalias

--- pari/install.sh
+++ pari/install.sh
@@ -2,7 +2,11 @@
 
 make install
 
-cd $PREFIX
+install -m 755 -d "$PREFIX/etc/"
+install -m 644 "misc/gprc.dft" "$PREFIX/etc/"
+install -m 644 "misc/gpalias" "$PREFIX/etc/"
+
+cd "$PREFIX"
 rm lib/pari/pari.cfg
 mv bin/gp-2.7 bin/gp-2.7-real
-install -m 755 $BASEDIR/pari/gp.sh bin/gp-2.7
+install -m 755 "$BASEDIR/pari/gp.sh" bin/gp-2.7
