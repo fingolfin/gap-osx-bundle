0b014e4ede575c223e52b14e5e0c9bedb626d4bf
Max Horn <max@quendi.de>
2014-10-19 16:39:36 +0200

    add some missing deps on autotools

--- gap/pkg/NormalizInterface/pkg.mk
+++ gap/pkg/NormalizInterface/pkg.mk
@@ -1,3 +1,6 @@
 GAP_PACKAGES += NormalizInterface
 
 gap/pkg/NormalizInterface: normaliz
+
+# We run autogen.sh, so we need the autotools
+gap/pkg/NormalizInterface: autoconf automake libtool
--- gap/pkg/SingularInterface/pkg.mk
+++ gap/pkg/SingularInterface/pkg.mk
@@ -1,3 +1,6 @@
 GAP_PACKAGES += SingularInterface
 
 gap/pkg/SingularInterface: singular
+
+# We run autogen.sh, so we need the autotools
+gap/pkg/SingularInterface: autoconf automake libtool
