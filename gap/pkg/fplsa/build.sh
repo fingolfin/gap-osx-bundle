b0814b23961592ba2d81039367dd359080f8c105
Max Horn <max@quendi.de>
2014-10-23 12:45:19 +0200

    fplsa: try to be compiler agnostic

--- gap/pkg/fplsa/build.sh
+++ gap/pkg/fplsa/build.sh
@@ -4,5 +4,5 @@ sed -i '' 's/malloc.h/string.h/' src/fplsa4.c
 ./configure
 make CC="gcc -O2 "
 
-mkdir bin/x86_64-apple-darwin10.0.0-clang-default64
-mv bin/fplsa4 bin/x86_64-apple-darwin10.0.0-clang-default64/
+mkdir bin/x86_64-apple-darwin10.0.0-$CC-default64
+mv bin/fplsa4 bin/x86_64-apple-darwin10.0.0-$CC-default64/
