051f6d18c0e139248e17ea7b1e6c7583a770e123
Max Horn <max@quendi.de>
2017-08-31 23:59:01 +0200

    Remove cmake: not needed anymore

--- cmake/compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -ev
-
-# Use a custom prefix -- we do not want to ship this package to the
-# user, but rather only use it as a build tool.
-#export MACOSX_DEPLOYMENT_TARGET=""
-./bootstrap --prefix=$TOOLSDIR
-make
